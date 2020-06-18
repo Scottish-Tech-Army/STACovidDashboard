2a63475042e44d48f715dfb9778d629b9c56bd3e
Rebecca Still <beckystill@hotmail.com>
2020-06-18 16:55:24 +0100

    refactor PercentTestsChart and add tests for component

--- homebrew_challenge/src/components/PercentTestsChart.jsx
+++ homebrew_challenge/src/components/PercentTestsChart.jsx
@@ -2,66 +2,74 @@ import React, { useEffect, useRef, useState } from 'react';
 import Chart from 'chart.js';
 import './PercentTestsChart.css';
 
-const chartConfig = {
-  type: 'line',
-      data: {
-          datasets: [{
-              label: '% of Positive Tests',
-              data: [{
-                t: new Date(2020, 3, 6),
-                y: 5
-              },
-              {
-                t: new Date(2020, 3, 7),
-                y: 7
-              },
-              {
-                t: new Date(2020, 3, 8),
-                y: 12
-              },
-              {
-                t: new Date(2020, 3, 9),
-                y: 16
-              },
-              {
-                t: new Date(2020, 3, 10),
-                y: 25
-              },
-              {
-                t: new Date(2020, 3, 11),
-                y: 22
-              }]
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true,
-                      maxTicksLimit: 100
-                  },
-              }],
-              xAxes: [{
-                type: 'time',
-                distribution: 'series'
-              }]
-          }
-      }
+const defaultInputData = {
+  t: [
+    "2020-04-06",
+    "2020-04-07",
+    "2020-04-08",
+    "2020-04-09",
+    "2020-04-10",
+    "2020-04-11"
+  ],
+  y: [
+    5,
+    7,
+    12,
+    16,
+    25,
+    22
+  ]
 }
 
-const PercentTestsChart = () => {
+const PercentTestsChart = ({inputData=defaultInputData}) => {
   const chartContainer = useRef(null);
   const [chartInstance, setChartInstance] = useState(null);
+  const createSeriesData = ({t,y}) => {
+    if (t.length !== y.length) {
+      throw new Error("Array lengths don't match");
+    }
+    return t.map((k, i) => {
+      return {t:Date.parse(k), y:y[i]}
+    });
+  }
+
+  const chartConfig = (inputData) => {
+    return {
+    type: 'line',
+        data: {
+            datasets: [{
+                label: '% of Positive Tests',
+                data: createSeriesData(inputData)
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        maxTicksLimit: 100
+                    },
+                }],
+                xAxes: [{
+                  type: 'time',
+                  distribution: 'series'
+                }]
+            }
+        }
+      }
+  }
 
   useEffect(() => {
     if (chartContainer && chartContainer.current) {
-      const newChartInstance = new Chart(chartContainer.current, chartConfig);
+      const newChartInstance = new Chart(chartContainer.current, chartConfig(inputData));
       setChartInstance(newChartInstance);
     }
-  }, [chartContainer]);
+  }, [inputData, chartContainer]);
+
+
 
   return (
-    <div class="chart-container">
+    <div className="chart-container">
       <canvas
         ref={chartContainer}/>
     </div>
