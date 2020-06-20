d1dc06e51c9b853ff85f616b28fa1f993d133ba1
Rebecca Still <beckystill@hotmail.com>
2020-06-20 23:36:34 +0100

    fix scale for blobs

--- homebrew_challenge/src/components/GeoHeatMap/GeoHeatMap.jsx
+++ homebrew_challenge/src/components/GeoHeatMap/GeoHeatMap.jsx
@@ -4,16 +4,88 @@ import './GeoHeatMap.css';
 
 const defaultInputData = [
   {
-    area: 'Aberdeen City',
-    lat: 57.1667,
-    lng: -2.1667,
-    totalCases: 400
+    area: 'Ayrshire and Arran',
+    lat: 55.445,
+    lng: -4.975,
+    totalCases: 1249
   },
   {
-    area: 'Aberdeenshire',
+    area: 'Borders',
     lat: 57.2869,
     lng: -2.3816,
-    totalCases: 1000
+    totalCases: 345
+  },
+  {
+    area: 'Dumfries and Galloway',
+    lat: 55.0701,
+    lng: -3.6053,
+    totalCases: 284
+  },
+  {
+    area: 'Fife',
+    lat: 56.2082,
+    lng: -3.1495,
+    totalCases: 928
+  },
+  {
+    area: 'Forth Valley',
+    lat: 56.0253,
+    lng: -3.8490,
+    totalCases: 1055
+  },
+  {
+    area: 'Grampian',
+    lat: 57.1497,
+    lng: -2.0943,
+    totalCases: 1400
+  },
+  {
+    area: 'Greater Glasgow and Clyde',
+    lat: 55.8836,
+    lng: -4.3210,
+    totalCases: 4819
+  },
+  {
+    area: 'Highland',
+    lat: 57.4596,
+    lng: -4.2264,
+    totalCases: 373
+  },
+  {
+    area: 'Lanarkshire',
+    lat: 55.6736,
+    lng: -3.7820,
+    totalCases: 2698
+  },
+  {
+    area: 'Lothian',
+    lat: 55.9484,
+    lng: -3.2121,
+    totalCases: 3139
+  },
+  {
+    area: 'Orkney',
+    lat: 58.9809,
+    lng: -2.9605,
+    totalCases: 9
+  },
+  {
+    area: 'Shetland',
+    lat: 60.5297,
+    lng: -1.2659,
+    totalCases: 54
+  },
+  {
+    area: 'Tayside',
+    lat: 56.4620,
+    lng: -2.970,
+    totalCases: 1760
+  },
+  {
+    area: 'Eileanan Siar (Western Isles)',
+    lat: 57.1667,
+    lng: -7.0194,
+    totalCases: 7
   }
 ]
 
@@ -30,7 +102,7 @@ const GeoHeatMap = ({inputData=defaultInputData}) => {
   ));
 
   const calculateRadius = (totalCases) => {
-    return totalCases * 20;
+    return totalCases * 10;
   };
 
   const createSeeds = (baseLayer) => {
