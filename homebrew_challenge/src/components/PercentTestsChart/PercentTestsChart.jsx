90400b310284bad812674ac6c88be5178ab684f9
Donal Stewart <donal.stewart@bcs.org.uk>
2020-06-20 15:50:02 +0100

    Use fetched data from statistics website

--- homebrew_challenge/src/components/PercentTestsChart/PercentTestsChart.jsx
+++ homebrew_challenge/src/components/PercentTestsChart/PercentTestsChart.jsx
@@ -1,77 +1,154 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Chart from 'chart.js';
-import './PercentTestsChart.css';
+import React, { useEffect, useRef, useState } from "react";
+import Chart from "chart.js";
+import "./PercentTestsChart.css";
 
-const defaultInputData = {
-  t: [
-    "2020-04-06",
-    "2020-04-07",
-    "2020-04-08",
-    "2020-04-09",
-    "2020-04-10",
-    "2020-04-11"
-  ],
-  y: [
-    5,
-    7,
-    12,
-    16,
-    25,
-    22
-  ]
+const defaultSeriesData = [
+  { t: Date.parse("2020-04-06"), y: 5 },
+  { t: Date.parse("2020-04-07"), y: 7 },
+  { t: Date.parse("2020-04-08"), y: 12 },
+  { t: Date.parse("2020-04-09"), y: 16 },
+  { t: Date.parse("2020-04-10"), y: 25 },
+  { t: Date.parse("2020-04-11"), y: 22 },
+];
+
+const queryUrl = "http://statistics.gov.scot/sparql.csv";
+
+const query = `PREFIX qb: <http://purl.org/linked-data/cube#>
+PREFIX dim: <http://purl.org/linked-data/sdmx/2009/dimension#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT ?date ?shortValue ?count
+WHERE {
+   VALUES (?value ?shortValue) {
+    ( <http://statistics.gov.scot/def/concept/variable/testing-cumulative-people-tested-for-covid-19-positive> "positive" )
+    ( <http://statistics.gov.scot/def/concept/variable/testing-cumulative-people-tested-for-covid-19-total> "total" )
+  }
+  ?obs qb:dataSet <http://statistics.gov.scot/data/coronavirus-covid-19-management-information> .
+  ?obs dim:refArea <http://statistics.gov.scot/id/statistical-geography/S92000003> .
+  ?obs <http://statistics.gov.scot/def/dimension/variable> ?value .
+  ?obs <http://statistics.gov.scot/def/measure-properties/count> ?count .
+  ?obs dim:refPeriod ?perioduri .
+  ?perioduri rdfs:label ?date
+}`;
+
+// Exported for tests
+export function parseCsvData(csvData) {
+  var allTextLines = csvData.split(/\r\n|\n/);
+  var lines = [];
+
+  allTextLines.forEach((line) => {
+    if (line.length > 0) {
+      lines.push(line.split(",").map((s) => s.trim()));
+    }
+  });
+  lines.shift();
+
+  const positivesMap = new Map();
+  const totalsMap = new Map();
+  const dateSet = new Set();
+
+  lines.forEach(([date, countType, count], i) => {
+    if ("positive" === countType) {
+      positivesMap.set(date, Number(count));
+    } else if ("total" === countType) {
+      totalsMap.set(date, Number(count));
+    } else {
+      throw new Error("Unrecognised input: " + countType);
+    }
+
+    dateSet.add(date);
+  });
+
+  const dates = [...dateSet].sort();
+  const points = [];
+  dates.forEach((date) => {
+    const total = totalsMap.get(date);
+    const positive = positivesMap.get(date);
+    points.push({ t: Date.parse(date), y: total === 0 ? 0 : positive / total });
+  });
+
+  return points;
 }
 
-const PercentTestsChart = ({inputData=defaultInputData}) => {
-  const chartContainer = useRef(null);
-  const [chartInstance, setChartInstance] = useState(null);
-  const createSeriesData = ({t,y}) => {
-    if (t.length !== y.length) {
-      throw new Error("Array lengths don't match");
-    }
-    return t.map((k, i) => {
-      return {t:Date.parse(k), y:y[i]}
-    });
-  }
+const PercentTestsChart = () => {
+  const chartContainer = useRef();
+  const chartInstance = useRef(null);
 
-  const chartConfig = (inputData) => {
-    return {
-    type: 'line',
-        data: {
-            datasets: [{
-                label: '% of Positive Tests',
-                data: createSeriesData(inputData)
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        maxTicksLimit: 100
-                    },
-                }],
-                xAxes: [{
-                  type: 'time',
-                  distribution: 'series'
-                }]
-            }
-        }
-      }
-  }
+  const [seriesData, setSeriesData] = useState(defaultSeriesData);
+  const [dataFetched, setDataFetched] = useState(false);
 
   useEffect(() => {
-    if (chartContainer && chartContainer.current) {
-      const newChartInstance = new Chart(chartContainer.current, chartConfig(inputData));
-      setChartInstance(newChartInstance);
+    function createChart(chartRef) {
+      const chart = new Chart(chartRef, {
+        type: "line",
+        data: {
+          datasets: [
+            {
+              label: "% of Positive Tests",
+              data: seriesData,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  maxTicksLimit: 100,
+                },
+              },
+            ],
+            xAxes: [
+              {
+                type: "time",
+                distribution: "series",
+              },
+            ],
+          },
+        },
+      });
+      console.log("Created chart");
+      return chart;
     }
-  }, [inputData, chartContainer]);
 
+    function updateChart() {
+      console.log(JSON.stringify(seriesData));
+      chartInstance.current.data.datasets[0].data = seriesData;
+      chartInstance.current.update();
+      console.log("Updated chart");
+    }
 
+    // Only attempt to fetch data once
+    if (!dataFetched) {
+      const form = new FormData();
+      form.append("query", query);
+      fetch(queryUrl, {
+        method: "POST",
+        body: form,
+      })
+        .then((res) => res.text())
+        .then((csvData) => {
+          console.log(csvData);
+          setSeriesData(parseCsvData(csvData));
+          setDataFetched(true);
+        })
+        .catch((error) => {
+          console.error(error);
+          setDataFetched(true);
+        });
+    }
+
+    if (chartInstance.current === null) {
+      const myChartRef = chartContainer.current.getContext("2d");
+      chartInstance.current = createChart(myChartRef);
+    } else {
+      updateChart();
+    }
+  }, [dataFetched, seriesData]);
 
   return (
     <div className="chart-container">
-      <canvas
-        ref={chartContainer}/>
+      <canvas ref={chartContainer} />
     </div>
   );
 };
--- homebrew_challenge/src/components/PercentTestsChart/PercentTestsChart.test.js
+++ homebrew_challenge/src/components/PercentTestsChart/PercentTestsChart.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PercentTestsChart from "./PercentTestsChart";
+import PercentTestsChart, { parseCsvData } from "./PercentTestsChart";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 
@@ -8,6 +8,7 @@ beforeEach(() => {
   // setup a DOM element as a render target
   container = document.createElement("div");
   document.body.appendChild(container);
+  fetch.resetMocks();
 });
 
 afterEach(() => {
@@ -17,33 +18,64 @@ afterEach(() => {
   container = null;
 });
 
-it("PercentTestsChart renders default data", () => {
-  act(() => {
-    render(<PercentTestsChart />, container);
-  });
-  const canvas = container.querySelector(".chart-container canvas");
-  expect(canvas).not.toBeNull();
-});
-
-it("Test PercentTestsChart throw errors when arrays don't match", () => {
+it("PercentTestsChart renders default data when fetch fails", async () => {
+  fetch.mockReject(new Error("fetch failed"));
   // Suppress console error message
   spyOn(console, "error");
-  
-  expect(() => {
-    act(() => {
-      render(<PercentTestsChart inputData={badInputData} />, container);
-    });
-  }).toThrow("Array lengths don't match");
+
+  await act(async () => {
+    render(<PercentTestsChart />, container);
+  });
+
+  const canvas = container.querySelector(".chart-container canvas");
+  expect(canvas).not.toBeNull();
+  expect(fetch.mock.calls.length).toEqual(1);
 });
 
-const badInputData = {
-  t: [
-    "2020-04-06",
-    "2020-04-07",
-    "2020-04-08",
-    "2020-04-09",
-    "2020-04-10",
-    "2020-04-11",
-  ],
-  y: [5, 7, 12, 16, 25, 22, 15],
-};
+it("PercentTestsChart renders dynamic fetched data", async () => {
+  fetch.mockResponse(csvData);
+
+  await act(async () => {
+    render(<PercentTestsChart />, container);
+  });
+
+  const canvas = container.querySelector(".chart-container canvas");
+  expect(canvas).not.toBeNull();
+  expect(fetch.mock.calls.length).toEqual(1);
+});
+
+it("parseCsvData", () => {
+  const expectedResult = [
+    { t: Date.parse("2020-03-02"), y: 1 / 815 },
+    { t: Date.parse("2020-03-03"), y: 1 / 915 },
+    { t: Date.parse("2020-03-04"), y: 3 / 1046 },
+    { t: Date.parse("2020-03-05"), y: 6 / 1256 },
+    { t: Date.parse("2020-03-06"), y: 11 / 1525 },
+  ];
+
+  expect(parseCsvData(csvData)).toEqual(expectedResult);
+});
+
+it("parseCsvData with bad count type", () => {
+  const badCsvData = `date,shortValue,count
+    2020-03-02,unknown,815`;
+
+  // Suppress console error message
+  spyOn(console, "error");
+
+  expect(() => {
+    parseCsvData(badCsvData);
+  }).toThrow("Unrecognised input: unknown");
+});
+
+const csvData = `date,shortValue,count
+2020-03-02,total,815
+2020-03-02,positive,1
+2020-03-05,positive,6
+  2020-03-05,total,1256
+  2020-03-03,total,915
+2020-03-03,positive,1
+2020-03-04,positive,3
+2020-03-04,total,1046
+2020-03-06,positive,11
+2020-03-06,total,1525`;
