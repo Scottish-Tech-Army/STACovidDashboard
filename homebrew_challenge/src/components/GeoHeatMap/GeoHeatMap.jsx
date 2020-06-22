1b5932d1cd389221d52f7b964d70259a13969d08
Rebecca Still <beckystill@hotmail.com>
2020-06-22 13:30:08 +0100

    remove totalDeaths from default input data

--- homebrew_challenge/src/components/GeoHeatMap/GeoHeatMap.jsx
+++ homebrew_challenge/src/components/GeoHeatMap/GeoHeatMap.jsx
@@ -7,99 +7,85 @@ const defaultInputData = [
     area: 'Ayrshire and Arran',
     lat: 55.445,
     lng: -4.575,
-    totalCases: 1249,
-    totalDeaths: 288
+    totalCases: 1249
   },
   {
     area: 'Borders',
     lat: 55.2869,
     lng: -2.7861,
-    totalCases: 345,
-    totalDeaths: 71
+    totalCases: 345
   },
   {
     area: 'Dumfries and Galloway',
     lat: 55.0701,
     lng: -3.6053,
-    totalCases: 284,
-    totalDeaths: 47
+    totalCases: 284
   },
   {
     area: 'Fife',
     lat: 56.2082,
     lng: -3.1495,
-    totalCases: 928,
-    totalDeaths: 195
+    totalCases: 928
   },
   {
     area: 'Forth Valley',
     lat: 56.0253,
     lng: -3.8490,
-    totalCases: 1055,
-    totalDeaths: 299
+    totalCases: 1055
   },
   {
     area: 'Grampian',
     lat: 57.1497,
     lng: -2.0943,
-    totalCases: 1400,
-    totalDeaths: 251
+    totalCases: 1400
   },
   {
     area: 'Greater Glasgow and Clyde',
     lat: 55.8836,
     lng: -4.3210,
-    totalCases: 4819,
-    totalDeaths: 1300
+    totalCases: 4819
   },
   {
     area: 'Highland',
     lat: 57.4596,
     lng: -4.2264,
-    totalCases: 373,
-    totalDeaths: 113
+    totalCases: 373
   },
   {
     area: 'Lanarkshire',
     lat: 55.6736,
     lng: -3.7820,
-    totalCases: 2698,
-    totalDeaths: 561
+    totalCases: 2698
   },
   {
     area: 'Lothian',
     lat: 55.9484,
     lng: -3.2121,
-    totalCases: 3139,
-    totalDeaths: 699
+    totalCases: 3139
   },
   {
     area: 'Orkney',
     lat: 58.9809,
     lng: -2.9605,
-    totalCases: 9,
-    totalDeaths: 2
+    totalCases: 9
   },
   {
     area: 'Shetland',
     lat: 60.1580,
     lng: -1.1659,
-    totalCases: 54,
-    totalDeaths: 7
+    totalCases: 54
   },
   {
     area: 'Tayside',
     lat: 56.4620,
     lng: -2.970,
-    totalCases: 1760,
-    totalDeaths: 307
+    totalCases: 1760
   },
   {
     area: 'Eileanan Siar (Western Isles)',
     lat: 57.1667,
     lng: -7.3594,
-    totalCases: 7,
-    totalDeaths: 0
+    totalCases: 7
   }
 ]
 
@@ -137,12 +123,9 @@ const GeoHeatMap = ({inputData=defaultInputData}) => {
       trackResize: false,
       touchZoom: false,
       scrollWheelZoom: false,
-      layers: [
-        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-          attribution:
-            '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        }),
-      ]
+      layers:
+        L.tileLayer('./MapofScotlandSHOWversion.png')
+
     });
 
     createSeeds(geoHeatMap);
