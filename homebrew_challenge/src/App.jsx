ad040b36f53270d63fdbbf4cc41fd3825c828623
Donal Stewart <donal.stewart@bcs.org.uk>
2020-06-23 22:48:13 +0100

    Restore other widgets to App

--- homebrew_challenge/src/App.jsx
+++ homebrew_challenge/src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import GeoHeatMap from "./components/GeoHeatMap/GeoHeatMap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import HeatmapContainer from "./components/HeatmapContainer/HeatmapContainer";
 import PercentTestsChart from "./components/PercentTestsChart/PercentTestsChart";
 import SingleValueBar from "./components/SingleValue/SingleValueBar";
-/*<SingleValueBar />
-<GeoHeatMap />
-<PercentTestsChart />*/
+
 const App = () => {
   return (
     <div className="App">
-
+      <SingleValueBar />
       <HeatmapContainer />
+      <PercentTestsChart />
     </div>
   );
 };
