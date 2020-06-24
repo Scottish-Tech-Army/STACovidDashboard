6c5429ff58aa089b2fe399abc35b6bcb6560d8c6
Donal Stewart <donal.stewart@bcs.org.uk>
2020-06-24 22:20:18 +0100

    Make the council areas vs cases option unselectable

--- homebrew_challenge/src/components/HeatmapContainer/HeatmapContainer.test.js
+++ homebrew_challenge/src/components/HeatmapContainer/HeatmapContainer.test.js
@@ -4,10 +4,18 @@ import Heatmap from "../HeatMap/Heatmap";
 import GeoHeatMap from "../GeoHeatMap/GeoHeatMap";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-import TestRenderer from "react-test-renderer";
 
-jest.mock('../HeatMap/Heatmap', () => () => 'Heatmap');
-jest.mock('../GeoHeatMap/GeoHeatMap', () => () => 'GeoHeatMap');
+var heatmapProps = null;
+var geoHeatmapProps = null;
+
+jest.mock("../HeatMap/Heatmap", () => (props) => {
+  heatmapProps = props;
+  return "Heatmap";
+});
+jest.mock("../GeoHeatMap/GeoHeatMap", () => (props) => {
+  geoHeatmapProps = props;
+  return "GeoHeatMap";
+});
 
 var container = null;
 beforeEach(() => {
@@ -24,18 +32,95 @@ afterEach(() => {
   container = null;
 });
 
+function checkButtonText(
+  expectedHealthBoards,
+  expectedCouncilAreas,
+  expectedDeaths,
+  expectedCases
+) {
+  expect(healthBoardsButton().textContent).toBe(expectedHealthBoards);
+  expect(councilAreasButton().textContent).toBe(expectedCouncilAreas);
+  expect(deathsButton().textContent).toBe(expectedDeaths);
+  expect(casesButton().textContent).toBe(expectedCases);
+}
+
+function checkHeatmapsProps(expectedAreaType, expectedValueType) {
+  expect(heatmapProps.areaType).toBe(expectedAreaType);
+  expect(heatmapProps.valueType).toBe(expectedValueType);
+  expect(geoHeatmapProps.areaType).toBe(expectedAreaType);
+  expect(geoHeatmapProps.valueType).toBe(expectedValueType);
+}
+
+const healthBoardsButton = () =>
+  container.querySelector("#datasetSelectionButtons #healthBoards");
+const councilAreasButton = () =>
+  container.querySelector("#datasetSelectionButtons #councilAreas");
+const deathsButton = () =>
+  container.querySelector("#datasetSelectionButtons #deaths");
+const casesButton = () =>
+  container.querySelector("#datasetSelectionButtons #cases");
+
+function click(button) {
+  act(() => {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+}
+
+function checkButtonsDisabled(casesButtonDisabled, councilAreasButtonDisabled) {
+    expect(casesButton().getAttribute("class").indexOf("disabled") > -1).toBe(casesButtonDisabled);
+    expect(councilAreasButton().getAttribute("class").indexOf("disabled") > -1).toBe(councilAreasButtonDisabled);
+}
+
 it("default render", async () => {
   fetch.mockReject(new Error("fetch failed"));
   // Suppress console error message
   spyOn(console, "error");
 
-  const testRenderer = TestRenderer.create(<HeatmapContainer />);
-  const testInstance = testRenderer.root;
+  act(() => {
+    render(<HeatmapContainer />, container);
+  });
 
-  expect(testInstance.findByType(Heatmap).props.valueType).toBe("deaths");
-  expect(testInstance.findByType(Heatmap).props.areaType).toBe("health-boards");
-  expect(testInstance.findByType(GeoHeatMap).props.valueType).toBe("deaths");
-  expect(testInstance.findByType(GeoHeatMap).props.areaType).toBe(
-    "health-boards"
+  checkButtonText("Health boards", "Council areas", "Deaths", "Cases");
+  checkButtonsDisabled(false, false);
+  checkHeatmapsProps("health-boards", "deaths");
+
+  click(councilAreasButton());
+  checkButtonText(
+    "Health boards",
+    "Council areas",
+    "Deaths",
+    "Cases [Data not available]"
   );
+  checkButtonsDisabled(true, false);
+  checkHeatmapsProps("council-areas", "deaths");
+
+  click(healthBoardsButton());
+  checkButtonText(
+    "Health boards",
+    "Council areas",
+    "Deaths",
+    "Cases"
+  );
+  checkButtonsDisabled(false, false);
+  checkHeatmapsProps("health-boards", "deaths");
+
+  click(casesButton());
+  checkButtonText(
+    "Health boards",
+    "Council areas [Data not available]",
+    "Deaths",
+    "Cases"
+  );
+  checkButtonsDisabled(false, true);
+  checkHeatmapsProps("health-boards", "cases");
+
+  click(deathsButton());
+  checkButtonText(
+    "Health boards",
+    "Council areas",
+    "Deaths",
+    "Cases"
+  );
+  checkButtonsDisabled(false, false);
+  checkHeatmapsProps("health-boards", "deaths");
 });
