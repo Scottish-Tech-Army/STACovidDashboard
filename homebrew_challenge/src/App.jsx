f7d0a1e3860891b36d536a925a4ca2ce8b053dd8
Donal Stewart <donal.stewart@bcs.org.uk>
2020-06-28 14:04:47 +0100

    Attempt at simplifying the layout

--- homebrew_challenge/src/App.jsx
+++ homebrew_challenge/src/App.jsx
@@ -61,42 +61,67 @@ const App = () => {
 
   return (
     <div className="App">
-      <Container fluid className="header">
-        <Row>
-          <Col id="logo">
-            <img id="logo" src="STALogo.png" alt="Scottish Tech Army Logo" />
-            <h1 className="header-title">Covid-19 Dashboard</h1>
+      <header>
+        <Container fluid className="header">
+          <Row id="page-title">
+            <Col>
+              <h1 className="header-title">
+                <img
+                  id="logo"
+                  src="STALogo.png"
+                  alt="Scottish Tech Army Logo"
+                />
+                Covid-19 Dashboard
+              </h1>
+            </Col>
+          </Row>
+          <Row
+            fluid
+            className="pt-3 d-none d-sm-flex justify-content-center align-items-center info-bar"
+          >
+            <Col>
+              <div className="info-bar">
+                <span id="icon">
+                  <FontAwesomeIcon
+                    icon={faInfoCircle}
+                    size="3x"
+                    color="#319bd5"
+                  />
+                </span>
+                <span id="message">
+                  Provisional dates for the relaxation of travel restrictions,
+                  restarting of the hospitality industry and reopening of
+                  hairdressers are among further route map measures announced
+                  today (Wednesday 24, June) by First Minister Nicola Sturgeon.
+                  For more information visit
+                  <a
+                    className="route-map-link"
+                    target="_blank"
+                    href="https://www.gov.scot/news/further-route-map-detail-announced/"
+                    rel="noopener noreferrer"
+                  >
+                    www.gov.scot/news/further-route-map-detail-announced
+                  </a>
+                  .
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </header>
+
+      <Container fluid>
+        <Row className="pt-3">
+          <Col>
+            <SingleValueBar />
           </Col>
         </Row>
-      </Container>
-
-      <Container
-        fluid
-        className="pt-3 d-none d-sm-flex justify-content-center align-items-center info-bar"
-      >
-        <span id="icon">
-          <FontAwesomeIcon icon={faInfoCircle} size="3x" color="#319bd5" />
-        </span>
-        <span id="message">
-          Provisional dates for the relaxation of travel restrictions,
-          restarting of the hospitality industry and reopening of hairdressers
-          are among further route map measures announced today (Wednesday 24,
-          June) by First Minister Nicola Sturgeon. For more information visit
-          <a
-            className="route-map-link"
-            target="_blank"
-            href="https://www.gov.scot/news/further-route-map-detail-announced/"
-            rel="noopener noreferrer"
-          >
-            www.gov.scot/news/further-route-map-detail-announced
-          </a>
-          .
-        </span>
-      </Container>
-      <SingleValueBar />
-      <hr className="full-width-hr" />
-      <Container fluid className="widgets_block">
         <Row>
+          <Col>
+            <hr className="full-width-hr" />
+          </Col>
+        </Row>
+        <Row className="widgets_block">
           <Col xs={12} md={8}>
             <Row>
               <Col>
@@ -143,60 +168,69 @@ const App = () => {
             </Row>
             <Row>
               <Col>
-                <TimeLine/>
+                <TimeLine />
               </Col>
             </Row>
           </Col>
         </Row>
+        <Row>
+          <Col>
+            <hr className="full-width-hr" />
+          </Col>
+        </Row>
       </Container>
 
-      <hr className="full-width-hr" />
+      <footer>
+        <Container className="text-center font-small blue pt-4">
+          <Row>
+            <Col>
+              <a
+                href="https://www.gov.scot/collections/coronavirus-covid-19-guidance/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Scottish Government guidance
+              </a>
+            </Col>
+            <Col>
+              <a
+                href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-poisoning/coronavirus-covid-19"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                NHS inform
+              </a>
+            </Col>
+            <Col>
+              <a
+                href="https://www.scottishtecharmy.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="final_logo.PNG"
+                  alt="Scottish Tech Army"
+                  width="270"
+                  height="50"
+                />
+              </a>
+            </Col>
+          </Row>
 
-      <footer className="page-footer font-small blue pt-4">
-        <Container fluid className="text-center text-md-left">
-
-          <Row style={{ margin: "0 0 20px 20px" }}>
-            <ul className="list-inline">
-              <li className="list-inline-item" style={{ marginRight: "200px" }}>
-                <a
-                  href="https://www.gov.scot/collections/coronavirus-covid-19-guidance/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Scottish Government guidance
-                </a>
-              </li>
-              <li className="list-inline-item" style={{ marginRight: "200px" }}>
-                <a
-                  href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-poisoning/coronavirus-covid-19"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  NHS inform
-                </a>
-              </li>
-              <li className="list-inline-item" style={{ marginRight: "200px" }}>
+          <Row>
+            <Col>
+              <div className="footer-copyright py-3">
+                © 2020 Copyright:&nbsp;
                 <a
                   href="https://www.scottishtecharmy.org/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                 <img src="final_logo.PNG" alt="Scottish Tech Army" width="270" height="50" />
+                  ScottishTechArmy.org
                 </a>
-              </li>
-            </ul>
+              </div>
+            </Col>
           </Row>
-
-          <div className="footer-copyright text-center py-3">
-            © 2020 Copyright:&nbsp;
-            <a
-              href="https://www.scottishtecharmy.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ScottishTechArmy.org
-            </a>
-          </div>
         </Container>
       </footer>
     </div>
