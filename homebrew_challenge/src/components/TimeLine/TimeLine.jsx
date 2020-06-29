6083a489b1e7a784ebe075920794b3b80c3a8eeb
Donal Stewart <donal.stewart@bcs.org.uk>
2020-06-29 22:42:44 +0100

    Much table css hackery to get a timeline

--- homebrew_challenge/src/components/TimeLine/TimeLine.jsx
+++ homebrew_challenge/src/components/TimeLine/TimeLine.jsx
@@ -1,36 +1,63 @@
 import React from "react";
+import { format } from "date-fns";
+import "./TimeLine.css";
+
+const events = [
+  { date: "2020-03-01", text: "First positive case in Scotland" },
+  {
+    date: "2020-03-11",
+    text: "First identified case of community transmission",
+  },
+  { date: "2020-03-13", text: "First confirmed death in Scotland" },
+  { date: "2020-03-24", text: "UK wide lockdown announced" },
+  { date: "2020-04-02", text: "Worldwide cases exceeds 1 Million" },
+  { date: "2020-04-10", text: "Global death toll surpasses 100,000" },
+  { date: "2020-04-20", text: "NHS Louisa Jordan Hospital opens" },
+  {
+    date: "2020-05-06",
+    text:
+      "First weekly reduction of registered deaths since Pandemic started in Scotland",
+  },
+  {
+    date: "2020-05-18",
+    text:
+      "Testing in Scotland available for everyone who is symptomatic over 5",
+  },
+  { date: "2020-05-21", text: "Scottish Covid-19 Roadmap published" },
+  {
+    date: "2020-05-29",
+    text: "Scotland moves to Phase 1 on the route map out of lockdown",
+  },
+  {
+    date: "2020-06-19",
+    text: "Scotland begins transition to Phase 2 of the Covid-19 roadmap",
+  },
+];
 
 const TimeLine = () => {
+  // The hoops we have to jump through to get a reliably non-wrapping string...
+  const nbsp = String.fromCharCode(160);
+  const dateFormatString = "dd" + nbsp + "MMM," + nbsp + "yyyy";
+
+  function getTableEntry({ date, text }) {
+    const dateString = format(new Date(date), dateFormatString);
+    return (
+      <tr>
+        <td className="date">{dateString}</td>
+        <td className="tick"></td>
+        <td className="text">
+          <span>{text}</span>
+        </td>
+      </tr>
+    );
+  }
+
   return (
-    <>
-      <h2>Scotland's Covid-19 Timeline</h2>
-      <ul>
-        <li>01/03/2020 - First positive case in Scotland</li>
-        <li>11/03/2020 - First identified case of community transmission</li>
-        <li>13/03/2020 - First confirmed death in Scotland</li>
-        <li>24/03/2020 - UK wide lockdown announced</li>
-        <li>02/04/2020 - Worldwide cases exceeds 1 Million</li>
-        <li>10/04/2020 - Global death toll surpasses 100,000</li>
-        <li>20/04/2020 - NHS Louisa Jordan Hospital opens</li>
-        <li>
-          06/05/2020 - First weekly reduction of registered death since Pandemic
-          started in Scotland
-        </li>
-        <li>
-          18/05/2020 - Testing in Scotland available for everyone who is
-          symptomatic over 5
-        </li>
-        <li>21/05/2020 - Scottish Covid-19 Roadmap published</li>
-        <li>
-          29/05/2020 - Scotland moves to Phase 1 on the route map out of
-          lockdown
-        </li>
-        <li>
-          19/06/2020 - Scotland begins transition to Phase 2 of the Covid-19
-          roadmap
-        </li>
-      </ul>
-    </>
+    <div className="events">
+      <table>
+        <tbody>{events.map(getTableEntry)}</tbody>
+      </table>
+    </div>
   );
 };
 
