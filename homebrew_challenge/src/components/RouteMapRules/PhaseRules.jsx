39832fe38ef70bcca8e08c85120c8611a5e7cbe1
Rebecca Still <beckystill@hotmail.com>
2020-07-08 17:02:11 +0100

    tidy up css and classNames applied on category cards

--- homebrew_challenge/src/components/RouteMapRules/PhaseRules.jsx
+++ homebrew_challenge/src/components/RouteMapRules/PhaseRules.jsx
@@ -8,40 +8,40 @@ const PhaseRules = ({category1, category2, category3, category4}) => {
         <div className="d-flex justify-content-center">
           <img className="category-image" src={category1[1]}/>
         </div>
-        <div>
-          {category1[0]}
+        <div className="category-text">
+          <span>{category1[0]}</span>
         </div>
       </div>
       <div className="category-card">
         <div className="d-flex justify-content-center">
           <img className="category-image" src={category2[1]}/>
         </div>
-        <div>
-          {category2[0]}
+        <div className="category-text">
+          <span>{category2[0]}</span>
         </div>
         </div>
         <div className="category-card">
           <div className="d-flex justify-content-center">
             <img className="category-image" src={category3[1]}/>
           </div>
-          <div className="d-flex justify-content-center">
-            {category3[0]}
+          <div className="category-text">
+            <span>{category3[0]}</span>
           </div>
         </div>
         <div className="category-card">
         <div className="d-flex justify-content-center">
           <img className="category-image" src={category4[1]}/>
         </div>
-        <div className="d-flex justify-content-center">
-          {category4[0]}
+        <div className="category-text">
+          <span>{category4[0]}</span>
         </div>
         </div>
         <div className="category-card">
         <div className="d-flex justify-content-center">
           <img className="category-image" src="./assets/more_info.png"/>
         </div>
-        <div>
-          View the full Scottish Government guidance for Phase [] here.
+        <div className="category-text">
+          <span>View the full Scottish Government guidance for Phase [] here.</span>
         </div>
       </div>
     </>
