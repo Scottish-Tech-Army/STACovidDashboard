fdf624c415ca56e3f5b6dcf2e1ebb5c173832667
Rebecca Still <beckystill@hotmail.com>
2020-07-08 20:08:47 +0100

    add Bootstrap to PhaseRules component

--- homebrew_challenge/src/components/RouteMapRules/PhaseRules.jsx
+++ homebrew_challenge/src/components/RouteMapRules/PhaseRules.jsx
@@ -1,61 +1,72 @@
 import React from 'react';
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const PhaseRules = ({categories}) => {
 
   return (
     <>
-      <div className="category-card">
-        <div className="d-flex justify-content-center">
+      <Row className="category-card">
+        <Col>
+        <Col className="d-flex justify-content-center">
           <img className="category-image" src={categories["cat1"]["icon"]}/>
-        </div>
-        <div className="category-text">
+        </Col>
+        <Col className="category-text">
           <span>{categories["cat1"]["text"]}</span>
-        </div>
-      </div>
+        </Col>
+        </Col>
+      </Row>
 
-      <div className="category-card">
-        <div className="d-flex justify-content-center">
+      <Row className="category-card">
+        <Col>
+        <Col className="d-flex justify-content-center">
           <img className="category-image" src={categories["cat2"]["icon"]}/>
-        </div>
-        <div className="category-text">
+        </Col>
+        <Col className="category-text">
           <span>{categories["cat2"]["text"]}</span>
-        </div>
-      </div>
+        </Col>
+        </Col>
+      </Row>
 
-      <div className="category-card">
-        <div className="d-flex justify-content-center">
+      <Row className="category-card">
+      <Col>
+        <Col className="d-flex justify-content-center">
           <img className="category-image" src={categories["cat3"]["icon"]}/>
-        </div>
-        <div className="category-text">
+        </Col>
+        <Col className="category-text">
           <span>{categories["cat3"]["text"]}</span>
-        </div>
-      </div>
+        </Col>
+        </Col>
+      </Row>
 
-      <div className="category-card">
-        <div className="d-flex justify-content-center">
+      <Row className="category-card">
+      <Col>
+        <Col className="d-flex justify-content-center">
           <img className="category-image" src={categories["cat4"]["icon"]}/>
-        </div>
-        <div className="category-text">
+        </Col>
+        <Col className="category-text">
           <span>{categories["cat4"]["text"]}</span>
-        </div>
-      </div>
+        </Col>
+        </Col>
+      </Row>
 
-      <div className="category-card">
+      <Row className="category-card">
+        <Col>
         <a
           className="route-map-link"
           href="https://www.gov.scot/publications/coronavirus-covid-19-what-you-can-and-cannot-do/pages/overview/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div className="d-flex justify-content-center">
+          <Col className="d-flex justify-content-center">
             <img className="category-image" src="./assets/more_info.png"/>
-          </div>
-          <div className="category-text">
+          </Col>
+          <Col className="category-text">
             <span>View the full Scottish Government guidance for {categories["title"]} here.</span>
-          </div>
+          </Col>
         </a>
-      </div>
+        </Col>
+      </Row>
     </>
   );
 };
