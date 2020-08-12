8eb5371d388429cdf94a038984eec81b168958d7
Rebecca Still <beckystill@hotmail.com>
2020-08-12 14:49:04 +0100

    Remove redundant tooltip for phase guidance

--- homebrew_challenge/src/components/RouteMapRules/RouteMapRules.jsx
+++ homebrew_challenge/src/components/RouteMapRules/RouteMapRules.jsx
@@ -3,14 +3,12 @@ import "./RouteMapRules.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import ToolTip from "../ToolTips/ToolTip";
 
 const RouteMapRules = () => {
   return (
     <Container fluid className="route-map-rules">
       <Row>
         <Col xs="12" className="current-phase mb-2">
-          <ToolTip tooltip="The description of the phases is summary rather than comprehensive: it will not include every aspect of the restrictions that is of concern." />
           <h3>
             We are currently in Phase 3 of the Scottish Government's COVID-19
             Routemap.
