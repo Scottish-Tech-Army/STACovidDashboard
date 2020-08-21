40837a573f33818d524cf1fb1e3366134ccc0bf5
Rebecca Still <beckystill@hotmail.com>
2020-08-21 14:44:10 +0100

    attempt to render five news items - WIP

--- homebrew_challenge/src/components/InfoBar/InfoBar.jsx
+++ homebrew_challenge/src/components/InfoBar/InfoBar.jsx
@@ -1,53 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { getLatestFiveNewsItems } from "../Utils/RssFeedUtils";
 import "./InfoBar.css";
 
 const InfoBar = () => {
-
   const [covidNews, setCovidNews] = useState(null);
   const rssFeedUrl = "data/newsScotGovRss.xml";
+
   useEffect(() => {
     fetch(rssFeedUrl, {
-      method: "GET",
+      method: "GET"
     })
-      .then((res) => res.text())
-      .then((rssFeed) => {
+      .then(res => res.text())
+      .then(rssFeed => {
         setCovidNews(getLatestFiveNewsItems(rssFeed));
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
   }, []);
 
+  try {
+    const covidNewsItems = covidNews.map((item, index) => {
+      console.log(item.title);
+      return (
+        <p key={index} className="news-item">
+          <a
+             className="news-item-link"
+             target="_blank"
+             href={item? item.link : "#"}
+             rel="noopener noreferrer"
+          >
+            {item? item.title : "no news yet"}
+          </a>
+          {" - "}
+          {item? item.description : "no news yet"}
+          {" | "}
+          {item? item.timestamp : "no news yet"}
+        </p>
+      );
+    });
+  } catch (error) {
+    return <>Loading...</>;
+  }
+
   return (
     <div className="info-bar">
       <span id="icon">
-        <img className="more-info-icon" src="./assets/more_info.png" alt=""/>
+        <img className="more-info-icon" src="./assets/more_info.png" alt="" />
       </span>
       <span>
         <p className="message">
           The latest news from
-            <a className="scot-gov-link link"
-                target="_blank"
-                href="https://news.gov.scot/news"
-                rel="noopener noreferrer">
-                news.gov.scot
-            </a>
-        </p>
-        <p className="news-item">
           <a
-             className="news-item-link"
-             target="_blank"
-             href={covidNews? covidNews.link : "#"}
-             rel="noopener noreferrer"
+            className="scot-gov-link link"
+            target="_blank"
+            href="https://news.gov.scot/news"
+            rel="noopener noreferrer"
           >
-            {covidNews? covidNews.title : "no news yet"}
+            news.gov.scot
           </a>
-          {" - "}
-          {covidNews? covidNews.description : "no news yet"}
-          {" | "}
-          {covidNews? covidNews.timestamp : "no news yet"}
         </p>
+        {covidNewsItems}
       </span>
     </div>
   );
