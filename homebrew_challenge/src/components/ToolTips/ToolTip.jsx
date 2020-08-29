fba99f17267f831c0a4566db1583460c09faae84
Rebecca Still <beckystill@hotmail.com>
2020-08-29 22:23:53 +0100

    re-introduce tooltips for single value tiles

--- homebrew_challenge/src/components/ToolTips/ToolTip.jsx
+++ homebrew_challenge/src/components/ToolTips/ToolTip.jsx
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import "./ToolTip.css";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import {Tooltip as ReactBootstrapTooltip} from "react-bootstrap";
+import { Tooltip as ReactBootstrapTooltip } from "react-bootstrap";
 
 function ToolTip({ id, tooltip }) {
   return (
-    <OverlayTrigger overlay={<ReactBootstrapTooltip>{tooltip}</ReactBootstrapTooltip>}>
+    <OverlayTrigger
+      overlay={<ReactBootstrapTooltip>{tooltip}</ReactBootstrapTooltip>}
+    >
       <div className="tooltip-icon">
         <FontAwesomeIcon icon={faQuestionCircle} size="1x" />
       </div>
