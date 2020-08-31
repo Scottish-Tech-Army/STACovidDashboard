b30a0fec2e24f1ffeba13e1c8bf7fcdbc5bf89d8
Rob Armitage <robert.c.armitage@gmail.com>
2020-08-31 17:44:04 +0100

    small changes

--- homebrew_challenge/src/App.jsx
+++ homebrew_challenge/src/App.jsx
@@ -500,27 +500,35 @@ const App = () => {
         <h1>Accessibility</h1>
         <p className="px-5">
           We want everyone who visits the Covid-19 Dashboard to feel welcome,
-          and able to find information whatever their circumstances
+          and able to find information whatever their circumstances.
         </p>
         <p className="px-5">To do this we've:</p>
         <ul>
           <li>
-            Designed the dashboard to be accessible and usable for everyone
+            Designed the dashboard to be accessible and usable for everyone.
           </li>
           <li>
             Provided alternative text and audio for media content where
-            appropriate
+            appropriate.
           </li>
         </ul>
         <hr />
         <h2>Confirmance statement</h2>
         <p className="px-5">
-          We're working towards meeting the AA standard of the Web COntent
-          Accessibility Guidelines (WCAG) 2.0.
+          We're working towards meeting the AA standard of the{" "}
+          <a
+            href="https://www.w3.org/TR/WCAG20/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Web Content Accessibility Guidelines (WCAG) 2.0
+          </a>
+          .
         </p>
+
         <p className="px-5">
           Automated testing has taken place against these standards and all
-          expectations are met under the directive
+          expectations are met under the directive.
         </p>
         <hr />
         <h2>Browsers</h2>
@@ -546,15 +554,7 @@ const App = () => {
         <h2>Screen Readers</h2>
         <p className="px-5">
           This website is compatible with modern screen readers and other
-          assistive technologies
-        </p>
-        <hr />
-        <h2>Data Sonification</h2>
-        <p className="px-5">
-          <i>
-            (Some copy needed from Eirini around the sonification piece to go
-            here)
-          </i>
+          assistive technologies.
         </p>
         <hr />
       </div>
