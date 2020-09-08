39919a9b73540a7cbd1222c62623082a6a57c84b
Rebecca Still <beckystill@hotmail.com>
2020-09-08 18:17:10 +0100

    fix formatting

--- homebrew_challenge/src/components/GeoHeatMap/RegionTypeSelector.jsx
+++ homebrew_challenge/src/components/GeoHeatMap/RegionTypeSelector.jsx
@@ -4,7 +4,7 @@ import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import "../../common.css";
 import {
   AREATYPE_COUNCIL_AREAS,
-  AREATYPE_HEALTH_BOARDS,
+  AREATYPE_HEALTH_BOARDS
 } from "../HeatmapDataSelector/HeatmapConsts";
 
 function RegionTypeSelector({ areaType, setAreaType }) {
@@ -20,21 +20,21 @@ function RegionTypeSelector({ areaType, setAreaType }) {
   return (
     <fieldset>
       <legend>Select Boundaries:</legend>
-    <ToggleButtonGroup
-      className="toggle-button-group"
-      name="areaType"
-      type="radio"
-      value={areaType}
-      vertical
-      onChange={setAreaType}
-    >
-      <ToggleButton id="healthBoards" value={AREATYPE_HEALTH_BOARDS}>
-        Health Boards
-      </ToggleButton>
-      <ToggleButton id="councilAreas" value={AREATYPE_COUNCIL_AREAS}>
-        Council Areas
-      </ToggleButton>
-    </ToggleButtonGroup>
+      <ToggleButtonGroup
+        className="toggle-button-group"
+        name="areaType"
+        type="radio"
+        value={areaType}
+        vertical
+        onChange={setAreaType}
+      >
+        <ToggleButton id="healthBoards" value={AREATYPE_HEALTH_BOARDS}>
+          Health Boards
+        </ToggleButton>
+        <ToggleButton id="councilAreas" value={AREATYPE_COUNCIL_AREAS}>
+          Council Areas
+        </ToggleButton>
+      </ToggleButtonGroup>
     </fieldset>
   );
 }
