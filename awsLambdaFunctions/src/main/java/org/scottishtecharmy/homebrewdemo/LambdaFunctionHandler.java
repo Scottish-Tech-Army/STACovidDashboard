8fae0a7c67611d3e913b3bf89eebff68328232b9
Donal Stewart <covidresponse@demonsoft.org>
2020-12-16 21:15:30 +0000

    Added tests by council area to datasets retrieved

--- awsLambdaFunctions/src/main/java/org/scottishtecharmy/homebrewdemo/LambdaFunctionHandler.java
+++ awsLambdaFunctions/src/main/java/org/scottishtecharmy/homebrewdemo/LambdaFunctionHandler.java
@@ -77,6 +77,8 @@ public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
                 OBJECTKEY_NHS_SCOT_TOTAL_COUNCIL_AREAS_LAST_MODIFIED, NHS_SCOT_TOTAL_COUNCIL_AREAS_URL);
         storeNhsScotData(context, OBJECTKEY_NHS_SCOT_TOTAL_HEALTH_BOARDS,
                 OBJECTKEY_NHS_SCOT_TOTAL_HEALTH_BOARDS_LAST_MODIFIED, NHS_SCOT_TOTAL_HEALTH_BOARDS_URL);
+        storeNhsScotData(context, OBJECTKEY_NHS_SCOT_TESTS_COUNCIL_AREAS,
+                OBJECTKEY_NHS_SCOT_TESTS_COUNCIL_AREAS_LAST_MODIFIED, NHS_SCOT_TESTS_COUNCIL_AREAS_URL);
         storeNhsScotData(context, OBJECTKEY_NHS_SCOT_TESTS_HEALTH_BOARDS,
                 OBJECTKEY_NHS_SCOT_TESTS_HEALTH_BOARDS_LAST_MODIFIED, NHS_SCOT_TESTS_HEALTH_BOARDS_URL);
     }
@@ -221,7 +223,8 @@ public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
             + "e8454cf0-1152-4bcb-b9da-4343f625dfef/download";
     private final static String NHS_SCOT_TESTS_HEALTH_BOARDS_URL = NHS_SCOT_URL_PREFIX
             + "8da654cd-293b-4286-96a4-b3ece86225f0/download";
-    
+    private final static String NHS_SCOT_TESTS_COUNCIL_AREAS_URL = NHS_SCOT_URL_PREFIX
+            + "3349540e-dc63-4d6d-a78b-00387b9aca50/download";
     private final static String OBJECTKEY_NHS_SCOT_DAILY_HEALTH_BOARDS_LAST_MODIFIED = OBJECT_FOLDER
             + "nhsDailyHealthBoardLastModified.txt";
     private final static String OBJECTKEY_NHS_SCOT_DAILY_COUNCIL_AREAS_LAST_MODIFIED = OBJECT_FOLDER
@@ -232,6 +235,8 @@ public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
             + "nhsTotalCouncilAreaLastModified.txt";
     private final static String OBJECTKEY_NHS_SCOT_TESTS_HEALTH_BOARDS_LAST_MODIFIED = OBJECT_FOLDER
             + "nhsTestsHealthBoardLastModified.txt";
+    private final static String OBJECTKEY_NHS_SCOT_TESTS_COUNCIL_AREAS_LAST_MODIFIED = OBJECT_FOLDER
+            + "nhsTestsCouncilAreaLastModified.txt";
 
     private final static String OBJECTKEY_NHS_SCOT_DAILY_HEALTH_BOARDS = OBJECT_FOLDER + "dailyHealthBoards.csv";
     private final static String OBJECTKEY_NHS_SCOT_DAILY_COUNCIL_AREAS = OBJECT_FOLDER + "dailyCouncilAreas.csv";
@@ -239,8 +244,8 @@ public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
             + "currentTotalsHealthBoards.csv";
     private final static String OBJECTKEY_NHS_SCOT_TOTAL_COUNCIL_AREAS = OBJECT_FOLDER
             + "currentTotalsCouncilAreas.csv";
-    private final static String OBJECTKEY_NHS_SCOT_TESTS_HEALTH_BOARDS = OBJECT_FOLDER
-            + "testsHealthBoards.csv";
+    private final static String OBJECTKEY_NHS_SCOT_TESTS_HEALTH_BOARDS = OBJECT_FOLDER + "testsHealthBoards.csv";
+    private final static String OBJECTKEY_NHS_SCOT_TESTS_COUNCIL_AREAS = OBJECT_FOLDER + "testsCouncilAreas.csv";
 
     private static final String BUCKET_NAME = "dashboard.aws.scottishtecharmy.org";
 
